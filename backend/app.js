98241a6531bb304f2fde3df77dce18887076bd7d
Jim Tupper <github@tupper.org.uk>
2018-01-20 14:26:42 +0000

    Basic tearoom functionality

---
+++ backend/app.js
@@ -0,0 +1,166 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var sassMiddleware = require('node-sass-middleware');
+var moment = require('moment');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var tearooms = {};
+getid = () => `${(Math.random() * 100000000)}`;
+mapmember = (m) => ({id: m.teaid, name: m.membername});
+
+function findOrCreateTearoom(name, broadcast) {
+  if (!tearooms[name]) {
+    tearooms[name] = {
+      name: name,
+      members: new Set(),
+      wantingtea: new Set(),
+      roundendsat: null,
+      broadcast: broadcast
+    };
+  }
+
+  return tearooms[name];
+}
+
+
+
+function addMemberToTearoom(tearoom, socket) {
+  if (!socket.teaid) { socket.teaid = getid(); }
+
+  tearoom.members.add(socket);
+  socket.join(tearoom.name);
+
+  tearoom.broadcast.emit('joined', {members: [...tearoom.members.values()].map(mapmember)});
+}
+
+function startTearoomRound(tearoom) {
+  if (tearoom && !tearoom.roundendsat) {
+    let endtime = moment().add(0.5, 'm');
+
+    tearoom.roundendsat = {
+      timer: setTimeout(getEndRoundHandler(tearoom), getMillisecondsToTime(endtime)),
+      endtime: endtime
+    };
+
+    tearoom.broadcast.emit('roundstarted', {timeleft: getMillisecondsToTime(tearoom.roundendsat.endtime)})
+  }
+}
+
+function randomNumberBetween0andX(x) {
+  return Math.floor(Math.random() * x);
+}
+
+function getMillisecondsToTime(targetTime) {
+  return Math.max(moment.duration(targetTime.diff(moment())).asMilliseconds(), 0);
+}
+
+function getEndRoundHandler(tearoom) {
+  return () => {
+
+    // choose tea maker
+    var teamakerIndex = randomNumberBetween0andX(tearoom.wantingtea.size);
+    var teamaker = [...tearoom.wantingtea][teamakerIndex];
+    var teafor = [...tearoom.wantingtea].filter(m => m != teamaker).map(mapmember);
+
+    // tell the room who's making tea
+    tearoom.broadcast.emit('roundcomplete', {teamaker: mapmember(teamaker), teafor: teafor});
+
+    tearoom.roundendsat = null;
+    tearoom.wantingtea.clear();
+  }
+}
+
+function addToTearound(tearoom, member) {
+  if (tearoom && tearoom.roundendsat && tearoom.members.has(member)) {
+    tearoom.wantingtea.add(member);
+
+    tearoom.broadcast.emit('inround', {wantingtea: [...tearoom.wantingtea.values()].map(mapmember), timeleft: getMillisecondsToTime(tearoom.roundendsat.endtime)})
+  }
+}
+
+function removeMemberFromTearoom(tearoom, member) {
+  if (tearoom && member) {
+    tearoom.members.delete(member);
+    tearoom.wantingtea.delete(member);
+
+    tearoom.broadcast.emit('joined', {members: [...tearoom.members.values()].map(mapmember)});
+  }
+}
+
+app.socket = (io) => {
+  io.set('origins', '*:*');
+  
+  io.on('connection', (socket) => {
+    var tearoom = null;
+
+    socket.membername = null;
+
+    socket.on('join', (data) => {
+      tearoom = findOrCreateTearoom(data.roomname, io.to(data.roomname));
+      socket.membername = data.membername;
+      addMemberToTearoom(tearoom, socket);
+    });
+
+    socket.on('wanttea', (data) => {
+      startTearoomRound(tearoom);
+
+      addToTearound(tearoom, socket);
+    });
+
+    socket.on('disconnect', () => {
+      removeMemberFromTearoom(tearoom, socket);
+    });
+
+    console.log('a user connected', socket.id);
+  });
+  console.log('Doing io things');
+}
+
+module.exports = app;
