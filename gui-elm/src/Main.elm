3d7c045927a4b7cd9cf24f16460056be476e97f1
Andi McLean <pandagrove@googlemail.com>
2018-01-21 21:30:11 +0000

    Increasedx the size of the logo

--- gui-elm/src/Main.elm
+++ gui-elm/src/Main.elm
@@ -309,7 +309,7 @@ navigation : Element Styles variation msg
 navigation = 
     row None
         [  spread, paddingXY 80 20]
-        [  image None [width (px 40), height (px 40)] {src = "img/logo.png", caption = ""}
+        [  image None [width (px 60), height (px 60)] {src = "img/logo.png", caption = ""}
         ,  row None 
             [ spacing 20, alignBottom]
             [ el NavOption [] (text "share")
