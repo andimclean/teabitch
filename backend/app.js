f2f677b22d76b0f2ca3b17e518cc627a74948b24
Jim Tupper <github@tupper.org.uk>
2018-01-22 20:32:05 +0000

    Service elm front-end

--- backend/app.js
+++ backend/app.js
@@ -28,9 +28,8 @@ app.use(sassMiddleware({
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'gui-elm', 'dist')));
 
-app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
